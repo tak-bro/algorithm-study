a5ac0bec13b1a1a257a1a4f3ad2c60ae3a6c6808
njir <tak@ubuntu14.04>
2015-05-25 15:26:17 +0900

    test

--- 1A.cpp
+++ 1A.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main(){
     unsigned long long n, m, a;
-    unsigned long long  answer;
+    unsigned long long answer;
     cin >> n >> m >> a;
     answer = ceil((n*1.0)/a) * ceil((m*1.0)/a);
     cout << answer<< endl;
