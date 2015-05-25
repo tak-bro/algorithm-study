788e127d2ace5f1a76c83e72910a502a745f7308
njir <tak@ubuntu14.04>
2015-05-25 15:30:18 +0900

    categorizing

---
+++ 1A-TheatreSquare/1A.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<math.h>
+
+using namespace std;
+
+int main(){
+    unsigned long long n, m, a;
+    unsigned long long  answer;
+    cin >> n >> m >> a;
+    answer = ceil((n*1.0)/a) * ceil((m * 1.0)/a);
+    cout << answer<< endl;
+
+    return 0;
+}
